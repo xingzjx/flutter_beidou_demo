f75c387051041c362c0e30633e8d954d9821ff14
zhujinxing <1181313247@qq.com>
2018-08-03 16:58:59 +0800

    找回密码页面UI设计

--- lib/find_pwd.dart
+++ lib/find_pwd.dart
@@ -63,7 +63,7 @@ class FindPwdPageState extends State<FindPwdPage> {
               )),
           new Container(
             alignment: Alignment.topLeft,
-            margin: new EdgeInsets.fromLTRB(30.0, 50.0, 0.0, 0.0),
+            margin: new EdgeInsets.fromLTRB(30.0, 28.0, 0.0, 0.0),
             child: new Text(
               "用户名",
               style: new TextStyle(fontSize: 12.0),
@@ -75,7 +75,7 @@ class FindPwdPageState extends State<FindPwdPage> {
                 keyboardType: TextInputType.phone,
                 style: new TextStyle(fontSize: 16.0, color: Colors.black),
                 decoration: new InputDecoration(
-                  hintText: '用户名',
+                  hintText: '请输入用户名',
                   border: InputBorder.none,
                 ),
               )),
@@ -106,7 +106,7 @@ class FindPwdPageState extends State<FindPwdPage> {
                   height: 8.0,
                 ),
                 new Container(
-                  width: 100.0,
+                  width: 200.0,
                   //height: 13.0,
                   child: new TextField(
                     keyboardType: TextInputType.phone,
@@ -115,6 +115,53 @@ class FindPwdPageState extends State<FindPwdPage> {
                       hintText: '手机号',
                       border: InputBorder.none,
                     ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          new Padding(
+              padding: new EdgeInsets.fromLTRB(30.0, 0.0, 30.0, 0.0),
+              child: new Divider(
+                height: 0.1,
+              )),
+          new Container(
+            alignment: Alignment.topLeft,
+            margin: new EdgeInsets.fromLTRB(30.0, 15.0, 0.0, 0.0),
+            child: new Text(
+              "验证码",
+              style: new TextStyle(fontSize: 12.0),
+            ),
+          ),
+          new Container(
+            margin: new EdgeInsets.fromLTRB(30.0, 0.0, 30.0, 0.0),
+            child: new Stack(
+              children: <Widget>[
+                new Container(
+                  margin: new EdgeInsets.fromLTRB(0.0, 0.0, 30.0, 0.0),
+                  child: new TextField(
+                    keyboardType: TextInputType.number,
+                    style: new TextStyle(fontSize: 14.0, color: Colors.black),
+                    decoration: new InputDecoration(
+                      hintText: '请输入验证码',
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+                new Align(
+                  alignment: Alignment.centerRight,
+                  child: new InkWell(
+                    child: new Container(
+                      child: new Text(
+                        "发送",
+                        style: new TextStyle(
+                            fontSize: 14.0, color: const Color(0xff498FDD)),
+                      ),
+                      margin: const EdgeInsets.fromLTRB(0.0, 15.0, 0.0, 0.0),
+                    ),
+                    onTap: () {
+                      setState(() {});
+                    },
                   ),
                 )
               ],
@@ -125,6 +172,113 @@ class FindPwdPageState extends State<FindPwdPage> {
               child: new Divider(
                 height: 0.1,
               )),
+          new Container(
+            alignment: Alignment.topLeft,
+            margin: new EdgeInsets.fromLTRB(30.0, 15.0, 0.0, 0.0),
+            child: new Text(
+              "用户名",
+              style: new TextStyle(fontSize: 12.0),
+            ),
+          ),
+          new Container(
+            margin: new EdgeInsets.fromLTRB(30.0, 0.0, 30.0, 0.0),
+            child: new Stack(
+              children: <Widget>[
+                new Container(
+                  margin: new EdgeInsets.fromLTRB(0.0, 0.0, 30.0, 0.0),
+                  child: new TextField(
+                    keyboardType: TextInputType.number,
+                    style: new TextStyle(fontSize: 14.0, color: Colors.black),
+                    decoration: new InputDecoration(
+                      hintText: '请输入验证码',
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+                new Align(
+                  alignment: Alignment.centerRight,
+                  child: new InkWell(
+                    child: new Container(
+                      child: new Text(
+                        "发送",
+                        style: new TextStyle(
+                            fontSize: 14.0, color: const Color(0xff498FDD)),
+                      ),
+                      margin: const EdgeInsets.fromLTRB(0.0, 15.0, 0.0, 0.0),
+                    ),
+                    onTap: () {
+                      setState(() {});
+                    },
+                  ),
+                )
+              ],
+            ),
+          ),
+          new Padding(
+              padding: new EdgeInsets.fromLTRB(30.0, 0.0, 30.0, 0.0),
+              child: new Divider(
+                height: 0.1,
+              )),
+          new Container(
+            alignment: Alignment.topLeft,
+            margin: new EdgeInsets.fromLTRB(30.0, 15.0, 0.0, 0.0),
+            child: new Text(
+              "用户名",
+              style: new TextStyle(fontSize: 12.0),
+            ),
+          ),
+          new Container(
+            margin: new EdgeInsets.fromLTRB(30.0, 0.0, 30.0, 0.0),
+            child: new Stack(
+              children: <Widget>[
+                new Container(
+                  margin: new EdgeInsets.fromLTRB(0.0, 0.0, 30.0, 0.0),
+                  child: new TextField(
+                    keyboardType: TextInputType.number,
+                    style: new TextStyle(fontSize: 14.0, color: Colors.black),
+                    decoration: new InputDecoration(
+                      hintText: '请输入验证码',
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+                new Container(
+                    margin: EdgeInsets.fromLTRB(0.0, 15.0, 0.0, 0.0),
+                    child: new Align(
+                      alignment: Alignment.centerRight,
+                      child: new Image.asset(
+                        'images/ic_pwd_show.png',
+                        width: 40.0,
+                        height: 24.0,
+                      ),
+                    )),
+              ],
+            ),
+          ),
+          new Padding(
+              padding: new EdgeInsets.fromLTRB(30.0, 0.0, 30.0, 0.0),
+              child: new Divider(
+                height: 0.1,
+              )),
+          new InkWell(
+            child: new Container(
+              width: MediaQuery.of(context).size.width,
+              decoration: new BoxDecoration(
+                color: const Color(0xff498fdd),
+                borderRadius: new BorderRadius.all(new Radius.circular(3.0)),
+              ),
+              child: new Center(
+                  child: new Text(
+                "确认修改",
+                style: new TextStyle(color: Colors.white, fontSize: 18.0),
+              )),
+              margin: const EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 0.0),
+              padding: const EdgeInsets.fromLTRB(0.0, 7.0, 0.0, 7.0),
+            ),
+            onTap: () {
+              setState(() {});
+            },
+          ),
         ],
       ),
       // This trailing comma makes auto-formatting nicer for build methods.
